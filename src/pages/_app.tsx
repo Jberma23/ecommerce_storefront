53f8b8a98892ce65d8e57aad11cf3f06633a1743
Jberma23 <Jesse.ben.berman@gmail.com>
2024-05-03 16:48:54 -0600

    removing error and errorboundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,24 +3,19 @@ import "../app/globals.css";
 import { Header } from "@/components/Header/Header";
 import { createContext, useContext, useState } from "react";
 
-import Error from "@/app/error";
-import ErrorBoundary from "@/app/ErrorBoundary";
-
 export const CurrencyContext = createContext("USD");
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [currency, setCurrency] = useState("USD");
   return (
     <>
-      <ErrorBoundary>
-        <CurrencyContext.Provider value={currency}>
-          <Header
-            defaultCurrency={currency}
-            HandleCurrencyChange={(event) => setCurrency(event.target.value)}
-          />
-          <Component {...pageProps} />
-        </CurrencyContext.Provider>
-      </ErrorBoundary>
+      <CurrencyContext.Provider value={currency}>
+        <Header
+          defaultCurrency={currency}
+          HandleCurrencyChange={(event) => setCurrency(event.target.value)}
+        />
+        <Component {...pageProps} />
+      </CurrencyContext.Provider>
     </>
   );
 }
